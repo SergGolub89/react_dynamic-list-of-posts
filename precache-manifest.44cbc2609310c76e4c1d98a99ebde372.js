99be361a7e1cb494221f4d7463d0481a0d98da1f
SerGolub89 <k3210389gsi@gmail.com>
2019-08-02 10:10:52 +0300

    Updates

--- precache-manifest.44cbc2609310c76e4c1d98a99ebde372.js
+++ precache-manifest.44cbc2609310c76e4c1d98a99ebde372.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3ba061e0bbc0fd60aa768c5f29c31a93",
+    "revision": "f350ac625989b8821d11ef346a53d950",
     "url": "/react_dynamic-list-of-posts/index.html"
   },
   {
-    "revision": "1cf5dd437ecbc93a115a",
-    "url": "/react_dynamic-list-of-posts/static/css/main.f39318f6.chunk.css"
+    "revision": "bfbd8279900ac11e0b3e",
+    "url": "/react_dynamic-list-of-posts/static/css/main.50d70958.chunk.css"
   },
   {
     "revision": "c52b036cd679506b634b",
     "url": "/react_dynamic-list-of-posts/static/js/2.398ce7b9.chunk.js"
   },
   {
-    "revision": "1cf5dd437ecbc93a115a",
-    "url": "/react_dynamic-list-of-posts/static/js/main.87627060.chunk.js"
+    "revision": "bfbd8279900ac11e0b3e",
+    "url": "/react_dynamic-list-of-posts/static/js/main.1480e868.chunk.js"
   },
   {
     "revision": "fc988510490290486853",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_dynamic-list-of-posts/precache-manifest.94c1ec92ad9d5e9aea1f15a6165ddf3d.js"
+  "/react_dynamic-list-of-posts/precache-manifest.44cbc2609310c76e4c1d98a99ebde372.js"
 );
 
 self.addEventListener('message', (event) => {
